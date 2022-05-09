aeafa66970056a0ab8aeb98222461bb88b9bbee9
Israel Sampaio <israelsampaio28@gmail.com>
2022-05-09 16:54:17 -0400

    refactor: rewrite Modal component to typescript

---
+++ src/components/Modal/Modal.interface.ts
@@ -0,0 +1,7 @@
+export type ModalProps = {
+  isOpen: boolean;
+  setIsOpen(
+    event: React.MouseEvent<Element, MouseEvent> | React.KeyboardEvent<Element>
+  ): void;
+  children: React.ReactNode;
+};
